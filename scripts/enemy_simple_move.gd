326ec5d405fdb4fcd3a9408979c345e41be87e6c
NateDevs <natedevs@gmail.com>
2026-02-05 12:31:12 -0800

    class 2-5

--- scripts/enemy_simple_move.gd
+++ scripts/enemy_simple_move.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Node2D
 
 const SPEED : float = 30.0
 var direction : int = -1
@@ -6,6 +6,7 @@ var direction : int = -1
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var death_timer: Timer = $DeathTimer
 
 func _process(delta: float) -> void:
 	if ray_cast_right.is_colliding():
@@ -19,3 +20,16 @@ func _process(delta: float) -> void:
 		print("Hit Left")
 		
 	position.x += SPEED * direction * delta
+	
+	
+
+
+func _on_hurtzone_body_entered(body: Node2D) -> void:
+	var player : Player = body as Player
+	player.velocity.y = player.JUMP_VELOCITY
+	animated_sprite_2d.play("death")
+	death_timer.start()
+
+
+func _on_death_timer_timeout() -> void:
+	queue_free()
